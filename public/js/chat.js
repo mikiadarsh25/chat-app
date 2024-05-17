f0b7a34385fe09d8f5d587adc12e92e10fdcb00a
Adarsh Prakash <57398892+mikiadarsh25@users.noreply.github.com>
2024-05-17 11:48:41 +0530

    Update chat.js fixed error bug

--- public/js/chat.js
+++ public/js/chat.js
@@ -110,6 +110,8 @@ $sendLocation.addEventListener("click", () => {
 });
 
 socket.emit("join", { username, room }, (error) => {
-  // alert(error);
-  // location.href = "/";
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
 });
